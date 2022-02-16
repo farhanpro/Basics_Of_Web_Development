6630d61f64bec14acfc88159e96688fc374385b2
farhanpro <shaihfarhan549@gmai.com>
2022-02-16 17:19:45 +0530

    Hooper Project Completed

--- React/Hoopermusic/music-player/src/components/AudioList/index.js
+++ React/Hoopermusic/music-player/src/components/AudioList/index.js
@@ -1,39 +1,42 @@
 import { backIcon } from '../assests';
+import {baseUrl} from '../../config';
 import './style.css';
 
-const AudioList = ({onBackButtonPress}) =>
-{
-    return(
-        <div className="audio-ls m-20">
-            <div onClick={onBackButtonPress} className="audio-ls-header">
-                <img src={backIcon}/>
-            </div>
-            <ul>
-                <li className="audio-ls-container mtb-10">
-                
-                <div className="audio-ls-item flex align-center ptb-10">
-                    <div className="audio-img">
-                        <img/>
-                    </div>
-                    <div className="audio-info mlr-10">
-                        <p>Beyond the line</p>
-                        <p>Beyond the line</p>
-                    </div>
-                </div>
-
-                <div className="audio-ls-item flex align-center">
-                    <div className="audio-img">
-                        <img/>
-                    </div>
-                    <div className="audio-info mlr-10">
-                        <p>Beyond the line</p>
-                        <p>Beyond the line</p>
-                    </div>
-                </div>
-                
-                </li>
-            </ul>
+const AudioList = ({ onBackButtonPress, audioList, onTrackSelect }) => {
+    console.log({ audioList });
+    return (
+      <div className="audio-ls p-20">
+        <div onClick={onBackButtonPress} className="audio-ls-header">
+          <img src={backIcon} />
         </div>
+  
+        <ul className="mtb-10">
+          {audioList.length ? (
+            audioList.map((item, index) => (
+              <li
+                onClick={() => onTrackSelect(index)}
+                key={index}
+                className="audio-ls-container"
+              >
+                <div className="audio-ls-item flex align-center ptb-5">
+                  <div className="audio-img">
+                    <img src={`${baseUrl}${item.avatar}`} />
+                  </div>
+                  <div className="audio-info mlr-10">
+                    <p>{item.title}</p>
+                    <p>{item.artist}</p>
+                  </div>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p style={{ textAlign: "center", fontSize: "16px" }}>
+              No Audio Available
+            </p>
+          )}
+        </ul>
+      </div>
     );
-}
-export default AudioList;
+  };
+  
+  export default AudioList;
--- React/Hoopermusic/music-player/src/components/Tabs/index.js
+++ React/Hoopermusic/music-player/src/components/Tabs/index.js
@@ -1,39 +1,69 @@
-import './style.css'
+import { useState } from "react";
+import { baseUrl } from "../../config";
+import "./style.css";
 
-const Tabs = props =>
-{
-     return(
-        <div className='tabs-container'>
-        <div className = 'tabs-header flex'>
-            <a className = "active" href="#commercial">
-                Commercial
+const Tabs = ({ tabData, onItemSelect }) => {
+  const [touched, setTouched] = useState(false);
+  const [active, setActive] = useState("");
+
+  const getActiveClass = (tab) => {
+    return touched
+      ? active === tab
+        ? "active"
+        : ""
+      : tabData[tab].isActive
+      ? "active"
+      : "";
+  };
+
+  console.log({ tabData });
+  return (
+    <div className="tab-container">
+      {/* tab header */}
+      <div className="tab-header flex">
+        {tabData &&
+          Object.keys(tabData).map((tab, index) => (
+            <a
+              key={index}
+              className={`${getActiveClass(tab)}`}
+              href={`#${tab}`}
+              onClick={() => {
+                setTouched(true);
+                setActive(tab);
+              }}
+            >
+              {tabData[tab].label}
             </a>
-            <a href="#freelicense">
-                Free License
-            </a>
-        </div>
-            {/* Tab  Content */}
-            <div className="tab-contents">
-                
-                <div className = "tab-content" id="commercial">
-                    <div className="content-wrapper flex justify-sb m-20">
-                        <div className="content-item">
-                            <img/>
-                        </div>
+          ))}
+      </div>
 
-                        <div className="content-item">
-                            <img/>
-                        </div>
-
+      {/* tab content */}
+      <div className="tab-contents">
+        {tabData &&
+          Object.keys(tabData).map((tab, index) => (
+            <div
+              key={index}
+              className={`tab-content ${getActiveClass(tab)}`}
+              id={`${tab}`}
+            >
+              <div className="content-wrapper flex justify-center m-20">
+                {tabData[tab].items.map((item, _index) => (
+                  <div onClick={() =>onItemSelect(tab, item.key)}key={_index}
+                    className="content-item m-10"
+                  >
+                    <div className="d-visilibity z-0"></div>
+                    <img src={`${baseUrl}music/${item.key}/${item.key}.jpg`} />
+                    <div className="content-label flex justify-center align-center">
+                      {item.label}
                     </div>
-                </div>
-
-                <div className = "tab-content" id="freelicense">
-                    Free License
-                </div>
-
+                  </div>
+                ))}
+              </div>
             </div>
-        </div>
-        );
-}
+          ))}
+      </div>
+    </div>
+  );
+};
+
 export default Tabs;
